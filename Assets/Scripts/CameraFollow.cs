669178e445d5d5a08bb606eac2226251ca56cc7a
DESKTOP-QKK2IP0\Jonat <Jonathan.Haudenschild@gmail.com>
2017-12-08 21:47:12 +0100

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private float distanceAway;
+    [SerializeField]
+    private float distanceUp;
+    [SerializeField]
+    private float smooth;
+    [SerializeField]
+    private Transform followedObject;
+    private Vector3 toPosition;
+
+	
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        //berechnen Position der Camera
+        //followedObject.position = position vom Player
+        //+ Distance von Camera zum Player
+        toPosition = followedObject.position + (Vector3.up * distanceUp - Vector3.forward * distanceAway);
+
+        // Vector3.Lerp(start pos, end pos, smooth);
+        transform.position = Vector3.Lerp(transform.position, toPosition , Time.deltaTime * smooth);
+
+        //Schaurichtung 
+        transform.LookAt(followedObject);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float runVelocity = 12;
+        public float rotateVelocity = 100;
+        public float jumpVelocity = 16;
+        public float distanceToGround = 1.3f;
+        public LayerMask ground;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string FORWARD_AXIS = "Vertical";
+        public string SIDEWAYS_AXIS = "Horizontal";
+        public string TURN_AXIS = "Mouse X";
+        public string JUMP_AXIS = "Jump";
+    }   
+
+    public MoveSettings moveSettings;
+    public InputSettings inputSettings;
+
+    private Rigidbody playerRigidbody;
+    private Vector3 velocity;
+    private Quaternion targetRotation;
+    private float forwardInput, sidewaysInput, turnInput, jumpInput;
+
+    //Spawn 
+    public Transform spawnPoint;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+       // transform.position = spawnPoint.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "DeathZone")
+        {
+            Spawn();
+        }   
+    }
+    void Awake()
+    {
+        velocity = Vector3.zero;
+        forwardInput = sidewaysInput = turnInput = jumpInput = 0;
+        targetRotation = transform.rotation;
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        GetInput();
+       // Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+
+
+    
+    void GetInput()
+    {
+        if (inputSettings.FORWARD_AXIS.Length != 0)
+            forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS);
+        if (inputSettings.SIDEWAYS_AXIS.Length != 0)
+            sidewaysInput = Input.GetAxis(inputSettings.SIDEWAYS_AXIS);
+        if (inputSettings.TURN_AXIS.Length != 0)
+            turnInput = Input.GetAxis(inputSettings.TURN_AXIS);
+        if (inputSettings.JUMP_AXIS.Length != 0)
+            jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIS);
+    }
+    void Run()
+    {
+        velocity.z = forwardInput * moveSettings.runVelocity;
+        velocity.x = sidewaysInput * moveSettings.runVelocity;
+        velocity.y = playerRigidbody.velocity.y;
+        //playerRigidbody.velocity = transform.TransformDirection(velocity);
+        playerRigidbody.AddForce(velocity);
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > 0)
+        {
+            targetRotation *= Quaternion.AngleAxis(moveSettings.rotateVelocity *
+            turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+    void Jump()
+    {
+        if (jumpInput != 0 && Grounded())
+        {
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x,
+            moveSettings.jumpVelocity, playerRigidbody.velocity.z);
+        }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 
+            moveSettings.distanceToGround, moveSettings.ground);
+    }
+}
