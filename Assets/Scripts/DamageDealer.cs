3ecbe75c141708a7e9f06d334e648b78bb87bf21
Tom <tommi1410@gmx.de>
2017-12-09 04:11:23 +0100

    eggs kriegen damage. kommt drauf an wo es erwischt wird.

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour {
+    public static bool PLAYER_1 = true;
+    public static bool PLAYER_2 = !PLAYER_1;
+
+
+
+    public bool associatedPlayer;
+    public float damageToDeal;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerBehaviour : MonoBehaviour
 {
+    // Starting health
+    public static float MAX_HEALTH = 100.0f;
+
+    // Define how much damage is received at the top/bottom of the egg here (relative to the center of the egg)
+    public static float TOP_DAMAGE_SCALAR = 0.4f;
+    public static float BOTTOM_DAMAGE_SCALAR = 0.6f;
+
+
     [System.Serializable]
     public class MoveSettings
     {
@@ -32,26 +40,109 @@ public class PlayerBehaviour : MonoBehaviour
     private Quaternion targetRotation;
     private float forwardInput, sidewaysInput, turnInput, jumpInput;
 
+
+    // Player specific attributes
+    public float relPivotPointY = 1.0f / 3.0f;
+    public float playerHealth = 100.0f;
+
+
     //Spawn 
     public Transform spawnPoint;
 
     private void Start()
     {
+        //print("start");
+        if(gameObject.tag == "player_1")
+        {
+            inputSettings.FORWARD_AXIS = "Vertical1";
+            inputSettings.SIDEWAYS_AXIS = "Horizontal1";
+        }
+        else if(gameObject.tag == "player_2")
+        {
+            inputSettings.FORWARD_AXIS = "Vertical";
+            inputSettings.SIDEWAYS_AXIS = "Horizontal";
+        }
         Spawn();
     }
 
     public void Spawn()
     {
-       // transform.position = spawnPoint.position;
+        // transform.position = spawnPoint.position;
+        playerHealth = MAX_HEALTH;
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collisionInfo)
     {
-        if (other.gameObject.tag == "DeathZone")
+        if (gameObject.tag == "DeathZone")
         {
             Spawn();
-        }   
+        }
+
+        // Determine enemy damage dealer
+        bool enemyDamageDealer;
+        if(gameObject.tag == "player_1")
+        {
+            enemyDamageDealer = DamageDealer.PLAYER_2;
+        }
+        else if(gameObject.tag == "player_2")
+        {
+            enemyDamageDealer = DamageDealer.PLAYER_1;
+        }
+        else
+        {
+            return;
+        }
+
+        // Get collision object as damage dealer object
+        DamageDealer dd = (DamageDealer)collisionInfo.collider.gameObject.GetComponent(typeof(DamageDealer));
+        if(dd == null)
+        {
+            return;
+        }
+
+        // If the object we collided to is assotiated with the enemy player, then we receive damage
+        if(dd.associatedPlayer == enemyDamageDealer)
+        {
+            // The direct damage the player receives from the DamageDealer object
+            //TODO: geschwindigkeit auch berücksichtigen
+            float receivedDamage = dd.damageToDeal * MAX_HEALTH;
+            //print("mag:" + (Mathf.Sqrt(collisionInfo.relativeVelocity.sqrMagnitude)));
+
+            // Adjust the damage, as the egg is more robust at the ends
+            ContactPoint cp = collisionInfo.contacts[0];
+            Vector3 localCollisionPos = gameObject.transform.InverseTransformPoint(cp.point);
+            float dmgScalar = calculateDamageScalar(localCollisionPos.y);
+            receivedDamage *= dmgScalar;
+
+            // Adjust health and check for negative health
+            playerHealth -= receivedDamage;
+
+            //print("Health: " + playerHealth);
+        }    
     }
+
+
+    float calculateDamageScalar(float localCollisionYPosition)
+    {
+        Vector3 boundingBoxSize = GetComponent<MeshFilter>().mesh.bounds.size;
+        Vector3 objectSize = Vector3.Scale(transform.localScale, boundingBoxSize);
+        float bottomLength = objectSize.y * relPivotPointY;
+        float topLength = objectSize.y - bottomLength;
+
+        if (localCollisionYPosition > 0.0f)
+        {
+            return 1.0f - ((1.0f - TOP_DAMAGE_SCALAR) * (localCollisionYPosition / topLength));
+        }  
+        else if(localCollisionYPosition < 0.0f)
+        {
+            return 1.0f + ((1.0f - TOP_DAMAGE_SCALAR) * (localCollisionYPosition / topLength));
+        }
+        else
+        {
+            return 1.0f;
+        }
+    }
+
     void Awake()
     {
         velocity = Vector3.zero;
@@ -67,6 +158,25 @@ public class PlayerBehaviour : MonoBehaviour
 
     void FixedUpdate()
     {
+        // This test creates spikes on the player 2 egg wherever the mouse is
+
+        /*
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // Casts the ray and get the first game object hit
+        Physics.Raycast(ray, out hit);
+        Debug.Log("This hit at " + hit.point + ", obj: " + hit.transform.tag);
+        if (hit.transform.tag == "player_2")
+        {
+            GameObject testSpike = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            testSpike.AddComponent<DamageDealer>();
+            DamageDealer dd = (DamageDealer)testSpike.GetComponent(typeof(DamageDealer));
+            dd.associatedPlayer = DamageDealer.PLAYER_1;
+            dd.damageToDeal = 0.3f;
+            testSpike.transform.position = hit.point;
+        }
+        */
+
         Run();
         Jump();
     }
@@ -84,6 +194,7 @@ public class PlayerBehaviour : MonoBehaviour
         if (inputSettings.JUMP_AXIS.Length != 0)
             jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIS);
     }
+
     void Run()
     {
         velocity.z = forwardInput * moveSettings.runVelocity;
@@ -102,6 +213,7 @@ public class PlayerBehaviour : MonoBehaviour
         }
         transform.rotation = targetRotation;
     }
+
     void Jump()
     {
         if (jumpInput != 0 && Grounded())
